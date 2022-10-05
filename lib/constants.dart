76af0482b68012826987b1f8e5c7cb1b18cd304d
wanrabbae <wancode.msg@gmail.com>
2022-10-05 10:35:19 +0700

    Init

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+// const kpink = Color.fromARGB(255, 65, 243, 80);
+const kpink = Color.fromARGB(255, 241, 29, 29);
+const kblue = Color(0xFF71b8ff);
+const kpurple = Color(0xFF9ba0fc);
+const korange = Color(0xFFffaa5b);
